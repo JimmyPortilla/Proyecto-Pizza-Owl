acec33cdc24fec459b969eddb94e5fd281696ace
japor <japor@DESKTOP-HMGKSI2.mshome.net>
2021-07-02 09:29:07 -0500

    Arreglo

--- src/PersonPack/DisplayFrame.java
+++ src/PersonPack/DisplayFrame.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package PersonPack;
 
 import antlr.ByteBuffer;
@@ -11,10 +7,7 @@ import com.hp.hpl.jena.rdf.model.RDFNode;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 
-/**
- *
- * @author Ali Saidi
- */
+
 public class DisplayFrame extends javax.swing.JFrame {
 
     /**
